e651ad9c675ee938b49137a35611d96b8b81269e
Chubak <chubakbidpaa@gmail.com>
2021-09-28 22:28:40 +0330

    done with regexes

--- main.go
+++ main.go
@@ -21,14 +21,23 @@ func init() {
 }
 
 const (
-	headerOne    = `(#{1})(.*)`
-	headeTwo     = `(#{2})(.*)`
-	headerThree  = `(#{3})(.*)`
-	headerFour   = `(#{4})(.*)`
-	headerFive   = `(#{5})(.*)`
-	headerFix    = `(#{6})(.*)`
-	asteriskText = `\*.*\*`
-	linkText     = `(\[.*\])(\((http)(?:s)?(\:\/\/).*\))`
-	imageFile    = `(\[(?:.*)?\])(\(.*(\.(jpg|png|gif|tiff|bmp))\))`
-	listText     = `((\*{1})(\s)(.*)(?:\n)?)+`
+	headerOne        = `(#{1})(.*)`
+	headeTwo         = `(#{2})(.*)`
+	headerThree      = `(#{3})(.*)`
+	headerFour       = `(#{4})(.*)`
+	headerFive       = `(#{5})(.*)`
+	headerFix        = `(#{6})(.*)`
+	boldItalicText   = `(\*|\_)+(\S+)(\*|\_)+`
+	linkText         = `(\[.*\])(\((http)(?:s)?(\:\/\/).*\))`
+	imageFile        = `(\!)(\[(?:.*)?\])(\(.*(\.(jpg|png|gif|tiff|bmp))(?:(\s\"|\')(\w|\W|\d)+(\"|\'))?\))`
+	listText         = `((\W{1})(\s)(.*)(?:\n)?)+`
+	underlineText    = `\_{1}.*\_{1}`
+	numberedListText = `((\d+\.)(\s)(.*)(?:\n)?)+`
+	blockQuote       = `((\>{1})(\s)(.*)(?:\n)?)+`
+	inlineCode       = "(\\`{1})(.*)(\\`{1})"
+	codeBlock        = "(\\`{3}\\n)(.*)(\\n\\`{3})"
+	horizontalLine   = `(\=|\-|\*){3}`
+	emailText        = `(\<{1})(\S+@\S+)(\>{1})`
+	boldItalicLink   = `(\*|\_)+(\[.*\])(\((http)(?:s)?(\:\/\/).*\))(\*|\_)+`
+	qText            = `(\"|\')(\w|\W|\d)+(\"|\')`
 )

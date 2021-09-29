7100e03a98e42a68d09fcd10e93ad89d6f6a277b
Chubak <chubakbidpaa@gmail.com>
2021-09-29 23:25:20 +0330

    bold italci parse and make

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/joho/godotenv"
+	"github.com/unidoc/unioffice/color"
 	"github.com/unidoc/unioffice/common/license"
+	"github.com/unidoc/unioffice/document"
+	"github.com/unidoc/unioffice/measurement"
+	"github.com/unidoc/unioffice/schema/soo/wml"
 )
 
 func init() {
@@ -39,4 +44,116 @@ const (
 	emailText        = `(\<{1})(\S+@\S+)(\>{1})`
 	qText            = `(\"|\')(\w|\W|\S)+(\"|\')`
 	tableText        = `(((\|)([a-zA-Z\d+\s#!@'"():;\\\/.\[\]\^<={$}>?(?!-))]+))+(\|))(?:\n)?((\|)(-+))+(\|)(\n)((\|)(\W+|\w+|\S+))+(\|$)`
+	threeUA	=	`(\_|\*){3}`
+	twoUA	=	`(\_|\*){2}`
+	oneUA	=	`(\_|\*){1}`
+	
 )
+
+
+func addBoldText(text string, para document.Paragraph) {
+	run := para.AddRun()
+	run.Properties().SetBold(true)
+	text = text[2:len(text) - 2]
+	run.AddText(text)
+
+}
+
+func addItalic(text string, para document.Paragraph) {
+	run := para.AddRun()
+	run.Properties().SetItalic(true)
+	text = text[2:len(text) - 2]
+	run.AddText(text)
+
+}
+
+
+func addItalicItalic(text string, para document.Paragraph) {
+	run := para.AddRun()
+	run.Properties().SetItalic(true)
+	run.Properties().SetBold(true)
+	text = text[3:len(text) - 3]
+	run.AddText(text)
+
+}
+
+func parseBold(pattern *regexp.Regexp, text string) (x int) {
+	twoUAP, err := regexp.Compile(twoUA)
+
+	if err != nil {
+		log.Fatal("Problem with compiling twoUA pattern!")
+	}
+
+	if pattern.MatchString(text) {
+		firstTwo := text[:2]
+		lastTwo := text[len(text) - 2:]
+
+		if twoUAP.MatchString(firstTwo) && twoUAP.MatchString(lastTwo) {
+			if firstTwo != lastTwo {
+				return 121
+			} else {
+				return 132
+			}
+		}
+	}
+
+	return 100
+}
+
+func parseItalic(pattern *regexp.Regexp, text string) (x int) {
+	oneUAP, err := regexp.Compile(oneUA)
+
+	if err != nil {
+		log.Fatal("Problem with compiling oneUA pattern!")
+	}
+
+	if pattern.MatchString(text) {
+		firstTwo := text[:2]
+		lastTwo := text[len(text) - 2:]
+
+		if oneUAP.MatchString(firstTwo) && oneUAP.MatchString(lastTwo) {
+			if firstTwo != lastTwo {
+				return 121
+			} else {
+				return 132
+			}
+		}
+	}
+
+	return 100
+}
+
+func parseBoldItalic(pattern *regexp.Regexp, text string) (x int) {
+	threeUAP, err := regexp.Compile(threeUA)
+
+	if err != nil {
+		log.Fatal("Problem with compiling threeUA pattern!")
+	}
+
+	if pattern.MatchString(text) {
+		firstTwo := text[:2]
+		lastTwo := text[len(text) - 2:]
+
+		if threeUAP.MatchString(firstTwo) && threeUAP.MatchString(lastTwo) {
+			if firstTwo != lastTwo {
+				return 121
+			} else {
+				return 132
+			}
+		}
+	}
+
+	return 100
+}
+
+
+
+func main() {
+	doc := document.New()
+	defer doc.Close()
+
+	para := doc.AddParagraph()
+
+	pattern, err := regexp.Compile(boldItalicText)
+
+}
